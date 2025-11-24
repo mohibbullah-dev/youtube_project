93da32521e59028d499950f9417321bdef418032
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-11-24 10:58:46 +0200

    feat: videoPlaylist schema done

---
+++ src/models/vidoe_playlist.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoPlayListSchem = new Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      index: true,
+      required: [true, "video_playList name is required"],
+    },
+    description: {
+      type: String,
+      index: true,
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const videoPlaylist = mongoose.model(
+  "videoPlaylist",
+  videoPlayListSchem
+);
