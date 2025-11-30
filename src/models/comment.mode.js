2c7756a65097f3e084e06fc910e0fa2cafb333c6
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-12-01 00:21:06 +0200

    feat: pagination plugin added

--- src/models/comment.mode.js
+++ src/models/comment.mode.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const commentSchema = new Schema(
   {
     content: {
@@ -21,5 +21,7 @@ const commentSchema = new Schema(
   },
   { timestamps: true }
 );
+
+commentSchema.plugin(mongooseAggregatePaginate);
 
 export const Comment = mongoose.model("Comment", commentSchema);
