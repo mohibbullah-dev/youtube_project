fa67867f6aa727447ecc5719807b006dd0afc6fd
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-12-01 22:44:35 +0200

    feat: watchLatter.model done

---
+++ src/models/watchLater.model.js
@@ -0,0 +1,21 @@
+import mongoose, { plugin, Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const watchLaterSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    videoId: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+watchLaterSchema.plugin(mongooseAggregatePaginate);
+
+const WatchLater = mongoose.model("WatchLater", watchLaterSchema);
