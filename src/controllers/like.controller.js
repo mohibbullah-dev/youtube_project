9480fb493eb6ed7fc84008df7ba13d2f96677cd0
Md Mohibbullah <mohammedhanif74612@gmail.com>
2025-12-04 16:44:25 +0200

    fix: likeControler fixed

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,21 +3,45 @@ import { LiKe } from "../models/like.model.js";
 import { apiError } from "../utils/apiError.js";
 import { apiResponse } from "../utils/apiResponse.js";
 import asyncHandler from "../utils/asyncHandler.js";
+import { sendNotification } from "../utils/notificatin.js";
+import { Video } from "../models/video.model.js";
+import { Tweet } from "../models/tweet.model.js";
 
 const createVideoLike = asyncHandler(async (req, res) => {
   const userId = req.user?.id;
   const videoId = req.params?.videoId;
-  if (!userId && !videoId)
+  if (!userId || !videoId)
     throw new apiError(400, "user and video are required");
+
+  const video = await Video.findById(videoId);
+  if (!video) throw new apiError(404, "video not found");
+
+  const exists = await LiKe.findOne({
+    likeOn: videoId,
+    owner: userId,
+  });
+
+  if (exists) null;
   const like = await LiKe.create({
     owner: userId,
     likeOn: videoId,
     modelOn: "Video",
   });
   if (!like) throw new apiError(500, "videoLike creation is failed");
+
+  await sendNotification(
+    userId,
+    video.owner,
+    "NEW_VIDEO_LIKE",
+    like._id,
+    "someone like on you video"
+  );
+
   return res
     .status(201)
     .json(new apiResponse(200, like, "like created succefully"));
+
+  return null;
 });
 const MyvideoLikes = asyncHandler(async (req, res) => {
   const { page, limit } = req.query;
@@ -63,8 +87,10 @@ const MyvideoLikes = asyncHandler(async (req, res) => {
 const createTweetLike = asyncHandler(async (req, res) => {
   const userId = req.user?.id;
   const tweetId = req.params?.tweetId;
-  if (!userId && !tweetId)
+  if (!userId || !tweetId)
     throw new apiError(400, "user and tweet are required");
+  const tweet = await Tweet.findById(tweetId);
+  if (!tweet) throw new apiError(404, "tweet not found");
 
   const like = await LiKe.create({
     owner: userId,
@@ -73,6 +99,15 @@ const createTweetLike = asyncHandler(async (req, res) => {
   });
 
   if (!like) throw new apiError(500, "tweetLike creation is failed");
+
+  await sendNotification(
+    userId,
+    tweetId,
+    "NEW_TWEET_LIKE",
+    like._id,
+    "someone liked on your tweet"
+  );
+
   return res
     .status(201)
     .json(new apiResponse(200, like, "tweetLike created succefully"));
@@ -123,13 +158,25 @@ const creatdCommentLike = asyncHandler(async (req, res) => {
   if (!userId && !commentId)
     throw new apiError(400, "user and comment are required");
 
+  const comment = await Comment.findById(commentId);
+
+  if (!comment) throw new apiError(404, "comment not found");
+
   const like = await LiKe.create({
     owner: userId,
     likeOn: commentId,
     modelOn: "Comment",
   });
-
   if (!like) throw new apiError(500, "commentLike creation is failed");
+
+  await sendNotification(
+    userId,
+    comment.owner,
+    "NEW_COMMENT_LIKE",
+    like._id,
+    "someone liked on your comment"
+  );
+
   return res
     .status(201)
     .json(new apiResponse(200, like, "commentLike created succefully"));
